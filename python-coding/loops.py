6c47c430549a5ceabffa315fc290dc5c79313296
AyeshaSangani <aysan1004@gmail.com>
2026-02-19 12:33:59 +0500

    Loops in Python

---
+++ python-coding/loops.py
@@ -0,0 +1,19 @@
+# Loops in Python
+
+# 1. For Loop
+
+# Iterarate over a list
+
+fruits = ["banana", "apple", "mango"]
+for fruit in fruits:
+    print(fruit)
+
+# Output: apple
+#         banana
+#         orange
+
+
+# Iterate over a string
+
+for char in "Python":
+    print(char)
