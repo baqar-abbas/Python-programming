08e152a756f1c2531cd71c8d828ffd11a2589613
Baqar Abbas <baqar.abbas@hotmail.com>
2026-08-03 18:51:53 +0500

    Name Mangling in Python

---
+++ python-coding/object-oriented-programming/encapsulation7.py
@@ -0,0 +1,23 @@
+# Python Encapsulation
+
+# Encapsulation is about protecting data inside a class.
+
+# Name Mangling
+# Name mangling is how Python implements private properties and methods.
+
+# When you use double underscores __, Python automatically renames it internally by adding _ClassName in front.
+
+# For example, __age becomes _Person__age.
+
+# Example
+# See how Python mangles the name:
+
+class Person:
+  def __init__(self, name, age):
+    self.name = name
+    self.__age = age
+
+p1 = Person("Emil", 30)
+
+# This is how Python mangles the name:
+print(p1._Person__age) # Not recommended!
