4926337e9dd31a86831f980f2941556f7f5ef1a8
Baqar Abbas <baqar.abbas@hotmail.com>
2026-08-04 22:51:20 +0500

    python inner classes i.e class within class

---
+++ python-coding/object-oriented-programming/inner_classes.py
@@ -0,0 +1,21 @@
+# Python Inner Classes
+# An inner class is a class defined inside another class. The inner class can access the properties and methods of the outer class.
+
+# Inner classes are useful for grouping classes that are only used in one place, making your code more organized.
+
+# Example
+# Create an inner class:
+
+class Outer:
+  def __init__(self):
+    self.name = "Outer Class"
+
+  class Inner:
+    def __init__(self):
+      self.name = "Inner Class"
+
+    def display(self):
+      print("This is the inner class")
+
+outer = Outer()
+print(outer.name)
