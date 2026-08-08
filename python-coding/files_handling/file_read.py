0efa0f6ee7db454cbdf37d2e2a997c866eb96dff
Baqar Abbas <baqar.abbas@hotmail.com>
2026-08-08 18:39:16 +0500

    Opening a file in python, read content and print in the console

---
+++ python-coding/files_handling/file_read.py
@@ -0,0 +1,4 @@
+f = open("demofile.txt", "rt")  # Open the file in read text mode
+content = f.read()  # Read the content of the file
+print(f"Content of the demofile.text below")
+print(content)  # Print the content to the console
