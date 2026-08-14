ad2bb247f662cb72e6e695cbc1949317ebe28f09
Baqar Abbas <baqar.abbas@hotmail.com>
2026-08-14 21:19:58 +0500

    installed numpy and create basic numpy array program

---
+++ python-coding/numPy_module/numPy_module.py
@@ -0,0 +1,20 @@
+# NumPy is a Python library.
+
+# NumPy is used for working with arrays.
+
+# NumPy is short for "Numerical Python".
+
+# Example
+# Create a NumPy array:
+
+import numpy as np
+
+arr = np.array([1, 2, 3, 4, 5])
+
+print(arr)
+
+print("Type of arr:", type(arr))
+
+print("Shape of arr:", arr.shape)
+
+print("Data type of arr:", arr.dtype)
