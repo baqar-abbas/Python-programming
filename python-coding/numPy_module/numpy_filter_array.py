8e0b063678509bb961418cd2d5a03217399d7fad
Baqar Abbas <baqar.abbas@hotmail.com>
2026-08-26 23:53:12 +0500

    Creating Filter Directly From Array

--- python-coding/numPy_module/numpy_filter_array.py
+++ python-coding/numPy_module/numpy_filter_array.py
@@ -69,3 +69,20 @@ newarr = arr[filter_arr]
 
 print(filter_arr)
 print(newarr)
+
+# Creating Filter Directly From Array
+# The above example is quite a common task in NumPy and NumPy provides a nice way to tackle it.
+
+# We can directly substitute the array instead of the iterable variable in our condition and it will work just as we expect it to.
+
+# Example
+# Create a filter array that will return only values higher than 42:
+
+arr = np.array([41, 42, 43, 44])
+
+filter_arr = arr > 42
+
+newarr = arr[filter_arr]
+
+print(filter_arr)
+print(newarr)
