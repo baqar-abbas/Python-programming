848142d8fb618cd686218348c8674844cdc56f41
Baqar Abbas <baqar.abbas@hotmail.com>
2026-08-29 22:57:10 +0500

    return a 2-D array with 3 rows, each containing 5 values

--- python-coding/numPy_module/data_distribution_programs.py
+++ python-coding/numPy_module/data_distribution_programs.py
@@ -20,3 +20,10 @@ print(x)
 # Even if we run the example above 100 times, the value 9 will never occur.
 
 # We can return arrays of any shape and size by specifying the shape in the size parameter.
+
+# Example
+# Same example as above, but return a 2-D array with 3 rows, each containing 5 values.
+
+x = random.choice([3, 5, 7, 9], p=[0.2, 0.3, 0.5, 0.0], size=(3, 5))
+
+print(x)
