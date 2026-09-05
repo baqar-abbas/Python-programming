7be805a9d54b789eee5a7907a4538c60f02cd745
Baqar Abbas <baqar.abbas@hotmail.com>
2026-09-05 20:59:25 +0500

    Normal and Binomial Distribution

--- python-coding/numPy_module/binomial_distrubution.py
+++ python-coding/numPy_module/binomial_distrubution.py
@@ -31,3 +31,21 @@ import seaborn as sns
 sns.displot(random.binomial(n=10, p=0.5, size=1000))
 
 plt.show()
+
+# Difference Between Normal and Binomial Distribution
+# The main difference is that normal distribution is continous whereas binomial is discrete, but if there are enough data points it will be quite similar to normal distribution with certain loc and scale.
+
+# Example
+
+from numpy import random
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+data = {
+  "normal": random.normal(loc=50, scale=5, size=1000),
+  "binomial": random.binomial(n=100, p=0.5, size=1000)
+}
+
+sns.displot(data, kind="kde")
+
+plt.show()
