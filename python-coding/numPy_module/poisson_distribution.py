d10e66e478f545732bf39ea41f583b9462d7fba6
Baqar Abbas <baqar.abbas@hotmail.com>
2026-09-05 21:51:08 +0500

    Visualization of Poisson Distribution

--- python-coding/numPy_module/poisson_distribution.py
+++ python-coding/numPy_module/poisson_distribution.py
@@ -18,3 +18,14 @@ x = random.poisson(lam=2, size=10)
 
 print(x)
 
+# Visualization of Poisson Distribution
+# Example
+
+from numpy import random
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+sns.displot(random.poisson(lam=2, size=1000))
+
+plt.show()
+
