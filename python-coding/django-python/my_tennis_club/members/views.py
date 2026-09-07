810b07297dba06afbeafbf503cb18c843e44169a
Baqar Abbas <baqar.abbas@hotmail.com>
2026-09-07 14:09:19 +0500

    Create template for members page to render the server returned html

--- python-coding/django-python/my_tennis_club/members/views.py
+++ python-coding/django-python/my_tennis_club/members/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
+# from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
 
 def members(request):
-    return HttpResponse("Welcome to the Members page of My Tennis Club! Hello World!")
+    template = loader.get_template('myfirst.html')
+    return HttpResponse(template.render())
